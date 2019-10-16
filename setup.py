f3e1d6cfb5f8d58290d7b6379b5255667d6f769a
InterfaceGUI <a124302710@gmail.com>
2019-10-16 17:16:38 +0800

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(name='isPLC-modbus',
-      version='1.0',
-      description='一個isPLC的通訊模組(modbus版)',
-      url='https://github.com/InterfaceGUI/pyisPLC-modbus.git',
-      author='LarsHagrid',
-      author_email='interfacegui@gmail.com',
-      license='GNU Lesser General Public License v3.0',
-      packages=['isPLC'],
-      install_requires=[
-          'crcmod>=1.7',
-          'pyserial>=3.4'
-      ],
-      zip_safe=False)
+setuptools.setup(
+    name='isPLC_Package',
+    version='1.0',
+    description='一個isPLC的通訊模組(modbus版)',
+    url='https://github.com/InterfaceGUI/pyisPLC-modbus',
+    author='LarsHagrid',
+    author_email='interfacegui@gmail.com',
+    license='GNU Lesser General Public License v3.0',
+    packages=['isPLC'],
+    install_requires=[
+        'crcmod>=1.7',
+        'pyserial>=3.4'
+    ],
+    zip_safe=False)
 
