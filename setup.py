f6a257b64950a377a1e21fad72d243cf65fc515e
InterfaceGUI <a124302710@gmail.com>
2019-10-16 17:06:22 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='isPLC-modbus',
       version='1.0',
       description='一個isPLC的通訊模組(modbus版)',
-      url='https://github.com/InterfaceGUI/pyisPLC-modbus',
+      url='https://github.com/InterfaceGUI/pyisPLC-modbus.git',
       author='LarsHagrid',
       author_email='interfacegui@gmail.com',
       license='GNU Lesser General Public License v3.0',
